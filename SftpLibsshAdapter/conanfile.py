a6712a2aa73639e7b83e8b04ca895daf9468151a
Alex Arnaiz <aarnaiz@werfen.com>
2021-05-10 12:24:57 +0200

    Added packet generation as post build event for compilation of SftpLibsshAdapter. SNOW-15908

--- SftpLibsshAdapter/conanfile.py
+++ SftpLibsshAdapter/conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conans import ConanFile
 
 class SFTPLibSSHAdapterConan(ConanFile):
@@ -11,3 +12,8 @@ class SFTPLibSSHAdapterConan(ConanFile):
 
 	def requirements(self):
 		self.requires("libssh/0.9.5@systelab/stable")
+		
+	def package(self):
+		self.copy("*.h", dst="include/SFTPLibSSHAdapter", src=".")
+		self.copy("SFTPLibSSHAdapter.lib", dst="lib", src=os.environ["CONAN_LIB_DIR"])
+		self.copy("SFTPLibSSHAdapter.pdb", dst="bin", src=os.environ["CONAN_LIB_DIR"])
